05cbc0ed4bfcc239fb921af1ec0a328ff8a277b3
lokossoufranck <lokossoufranck@gmail.com>
2022-06-11 07:11:35 +0100

    configuration of host

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,18 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+	plugins:[vue()],
+	https: true,
+
+	server:{
+		strictPort: true,
+		port:8100,
+		host:'3.137.151.244',
+		 hmr: {
+			   port: 8100,
+			   protocol: 'wss',
+			   clientPort: 443
+				},
+	}
+
 })
